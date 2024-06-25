4efc2de2fa3fc74db027a41629a40bab3d97aef8
Michiel de Jong <michiel@pondersource.com>
2024-06-25 23:34:50 +0200

    Fix GitHubReplica

--- src/github.ts
+++ src/github.ts
@@ -29,101 +29,6 @@ export interface GitHubCommentAdd {
   body: string;
 }
 
-export async function apiCall(
-  url: string,
-  method: string,
-  body?: string
-): Promise<any> {
-  if (typeof process.env.GITHUB_TOKEN !== "string") {
-    console.error("export GITHUB_TOKEN=...");
-    process.exit(1);
-  }
-  const fetchResult = await fetch(url, {
-    method,
-    headers: {
-      Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-    body,
-  });
-  return fetchResult.json();
-}
-export async function getDataOverNetwork(url: string): Promise<GitHubIssue[]> {
-  return apiCall(url, "GET");
-}
-
-export async function remoteCreate(url: string, data: object): Promise<string> {
-  const response = (await apiCall(
-    url,
-    "POST",
-    JSON.stringify(data, null, 2)
-  )) as { url: string };
-  return response.url;
-}
-
-export async function getData(filename: string, url: string): Promise<any> {
-  let issues: GitHubIssue[] = [];
-  try {
-    const buff = await fsPromises.readFile(filename);
-    issues = JSON.parse(buff.toString());
-    console.log(`Loaded ${filename}`);
-  } catch {
-    console.log(`Failed to load ${filename}, fetching over network`);
-    issues = await getDataOverNetwork(url);
-    await fsPromises.writeFile(
-      filename,
-      JSON.stringify(issues, null, 2) + "\n"
-    );
-    console.log(`Saved ${filename}`);
-  }
-  return issues;
-}
-
-export async function getIssues(filename: string): Promise<GitHubIssue[]> {
-  return getData(filename, `https://api.github.com/issues`);
-}
-
-export async function addIssue(issue: GitHubIssueAdd): Promise<string> {
-  return remoteCreate(`${issue.repository_url}/issues`, issue);
-}
-export async function addComment(comment: GitHubCommentAdd): Promise<string> {
-  return remoteCreate(`${comment.issue_url}/comments`, comment);
-}
-
-export async function addItem(item: Item, store: DataStore) {
-  if (item.type === "issue") {
-    return addIssue({
-      repository_url:
-        "https://api.github.com/federatedbookkeeping/task-tracking",
-      title: (item as Issue).title,
-      body: "",
-    });
-  }
-  if (item.type === "comment") {
-    const issueUrlCandidates = store.issueIdToIssueIds(
-      (item as Comment).issueId
-    );
-    for (let i = 0; i < issueUrlCandidates.length; i++) {
-      if (issueUrlCandidates[i].startsWith("gh_api_url:")) {
-        return addComment({
-          issue_url: issueUrlCandidates[i].substring("gh_api_url:".length),
-          body: (item as Comment).body,
-        });
-      }
-    }
-  }
-}
-
-export async function upsert(item: Item, store: DataStore): Promise<void> {
-  let ghApiUrl: string | undefined = item.identifiers.find((x: string) =>
-    x.startsWith("gh_api_url:")
-  );
-  if (typeof ghApiUrl === "undefined") {
-    ghApiUrl = await addItem(item, store);
-  }
-}
-
 export type GitHubReplicaSpec = {
   tokens: {
     [user: string]: string;
@@ -143,17 +48,19 @@ export class GitHubReplica {
     this.dataStore = dataStore;
   }
 
-  async apiCall(args: { url: string, method: string, body?: string, user?: string }): Promise<any> {
+  async apiCall(args: { url: string, method: string, body?: string, user: string }): Promise<any> {
     const headers = {
       Accept: "application/vnd.github+json",
       "X-GitHub-Api-Version": "2022-11-28",
     };
     if (typeof args.user === 'string') {
-      if (this.tokens[args.user] !== "string") {
+      if (typeof this.tokens[args.user] !== "string") {
+        // console.log(this.tokens);
         throw new Error(`No token available for user "${args.user}"`);
       }
       headers['Authorization'] = `Bearer ${this.tokens[args.user]}`;
     }
+    console.log('apiCall', args);
     const fetchResult = await fetch(args.url, {
       method: args.method,
       headers,
@@ -161,8 +68,8 @@ export class GitHubReplica {
     });
     return fetchResult.json();
   }
-  async getDataOverNetwork(url: string): Promise<GitHubIssue[]> {
-    return this.apiCall({ url, method: "GET" });
+  async getDataOverNetwork(url: string, user: string): Promise<GitHubIssue[]> {
+    return this.apiCall({ url, method: "GET", user });
   }
 
   async remoteCreate(user: string, url: string, data: object): Promise<string> {
@@ -176,7 +83,7 @@ export class GitHubReplica {
     return response.url;
   }
 
-  async getData(filename: string, url: string): Promise<any> {
+  async getData(filename: string, url: string, user: string): Promise<any> {
     let issues: GitHubIssue[] = [];
     try {
       const buff = await fsPromises.readFile(filename);
@@ -184,7 +91,7 @@ export class GitHubReplica {
       console.log(`Loaded ${filename}`);
     } catch {
       console.log(`Failed to load ${filename}, fetching over network`);
-      issues = await this.getDataOverNetwork(url);
+      issues = await this.getDataOverNetwork(url, user);
       await fsPromises.writeFile(
         filename,
         JSON.stringify(issues, null, 2) + "\n"
@@ -194,8 +101,8 @@ export class GitHubReplica {
     return issues;
   }
 
-  async getIssues(filename: string): Promise<GitHubIssue[]> {
-    return this.getData(filename, `https://api.github.com/issues`);
+  async getIssues(filename: string, user: string): Promise<GitHubIssue[]> {
+    return this.getData(filename, this.trackerUrl + `/issues`, user);
   }
 
   async addIssue(user: string, issue: GitHubIssueAdd): Promise<string> {
@@ -237,11 +144,11 @@ export class GitHubReplica {
       ghApiUrl = await this.addItem(user, item);
     }
   }
-  async sync() {
-    const docs: GitHubIssue[] = await getIssues('gh-data/issues.json');
+  async sync(user) {
+    const docs: GitHubIssue[] = await this.getIssues('gh-data/issues.json', user);
     let comments: GitHubComment[] = [];
     const commentFetches = docs.map(async doc => {
-      const issueComments = await getData(`gh-data/comments_${doc.node_id}.json`, doc.comments_url);
+      const issueComments = await this.getData(`gh-data/comments_${doc.node_id}.json`, doc.comments_url, user);
       comments = comments.concat(issueComments);
     });
     await Promise.all(commentFetches);
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import { GitHubIssue, getIssues, getData, GitHubComment } from "./github.js";
 import { DataStore } from "./data.js";
-import { convertComment, convertIssue } from "./cambria.js";
 import { getReplicas } from "./replicas.js";
 
 async function run(): Promise<void> {
   const dataStore = new DataStore();
   await dataStore.load('./data.json');
   const replicas = await getReplicas(dataStore);
-  await Promise.all(replicas.map(replica => replica.sync()));
+  await Promise.all(replicas.map(replica => replica.sync('michielbdejong')));
   await dataStore.save('./data.json');
 }
 
