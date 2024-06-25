db65ea014f52c75e09fe8ad0b49f383f0d488ef4
Michiel de Jong <michiel@pondersource.com>
2024-06-25 16:06:02 +0200

    Fetch comments

--- src/github.ts
+++ src/github.ts
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+const fsPromises = require("fs/promises");
 
 export interface GitHubIssue {
   node_id: string,
@@ -10,10 +11,10 @@ export interface GitHubIssue {
 // using the `gh` executable here since there seems to be no tsconfig.json
 // that can compile a project that uses both Octokit and DXOS.
 
-export async function getIssues(): Promise<GitHubIssue[]> {
+export async function getDataOverNetwork(url: string): Promise<GitHubIssue[]> {
   const result: { code: any; stdout: string; stderr: string } =
     await new Promise((resolve) => {
-      exec(`gh api /issues --method GET`, (code, stdout, stderr) => {
+      exec(`gh api ${url} --method GET`, (code, stdout, stderr) => {
         resolve({ code, stdout, stderr });
       });
     });
@@ -22,4 +23,23 @@ export async function getIssues(): Promise<GitHubIssue[]> {
   console.log("stderr from GH API call:", stderr);
   const text = stdout;
   return JSON.parse(text);
+}
+
+export async function getData(filename: string, url: string): Promise<any> {
+  let issues: GitHubIssue[] = [];
+  try {
+    const buff = await fsPromises.readFile(filename);
+    issues = JSON.parse(buff.toString());
+    console.log(`Loaded ${filename}`);
+  } catch {
+    console.log(`Failed to load ${filename}, fetching over network`);
+    issues = await getDataOverNetwork(url);
+    await fsPromises.writeFile(filename, JSON.stringify(issues, null, 2) + "\n");
+    console.log(`Saved ${filename}`);
+  }
+  return issues;
+}
+
+export async function getIssues(filename: string): Promise<GitHubIssue[]> {
+  return getData(filename, `/issues`);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import { getIssues } from "./github.js";
+import { GitHubIssue, getIssues, getData } from "./github.js";
 import { DataStore, Issue } from "./data.js";
 import { convertIssue } from "./cambria.js";
 
 async function run(): Promise<void> {
   const dataStore = new DataStore();
   await dataStore.load('./data.json');
-  const docs = await getIssues();
-  const promises = docs.map(async (doc) => {
+  const docs: GitHubIssue[] = await getIssues('gh-data/issues.json');
+  await Promise.all(docs.map(doc => getData(`gh-data/comments_${doc.node_id}.json`, doc.comments_url)));
+  const upserts = docs.map(async (doc) => {
     console.log('upserting doc', doc);
     dataStore.applyOperation({
       operationType: 'upsert',
       fields: await convertIssue(doc)
     });
   });
-  await Promise.all(promises);
+  await Promise.all(upserts);
   console.log(dataStore.items);
   await dataStore.save('./data.json');
 }
