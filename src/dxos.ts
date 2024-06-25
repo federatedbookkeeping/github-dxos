0044a0460d33526720825ce281dcfe281fcfc926
Michiel de Jong <michiel@pondersource.com>
2024-06-25 11:08:20 +0200

    trying to use DXOS invites

--- src/dxos.ts
+++ src/dxos.ts
@@ -1,11 +1,32 @@
-const dxos = require("@dxos/client");
+const { Client } = require("@dxos/client");
+const { Space } = require("@dxos/client/echo");
+const { InvitationEncoder } = require('@dxos/client/invitations');
 
-export async function initDxos() {
-  const client = new dxos.Client();
+export async function initDxos(): Promise<Client> {
+  const client = new Client();
+  console.log('DXOS initialize');
   await client.initialize();
-  // ensure an identity exists:
-  if (!client.halo.identity.get()) await client.halo.createIdentity();
-  // create a space:
-  const space = await client.spaces.create();
-  return { client, space };
+  if (!client.halo.identity.get()) {
+    console.log('creating DXOS identity');
+    await client.halo.createIdentity();
+  }
+  console.log('DXOS waiting for spaces to be ready');
+  await client.spaces.isReady.wait();
+  console.log('DXOS spaces ready');
+  return client;
+}
+export async function acceptInvite(client: Client, code: string, authCode: string): Promise<Space> {
+  const receivedInvitation = InvitationEncoder.decode(code);
+  const invitation = client.spaces.join(receivedInvitation);
+  await invitation.authenticate(authCode);
+  return client.spaces.get(invitation.get().spaceKey!)!;
+}
+
+export async function generateInvite(client: Client): Promise<{ space: object, code: string, authCode: string }> {
+  const space = client.spaces.default;
+  const invitation = space.share();
+  const code = InvitationEncoder.encode(invitation.get());
+  const authCode = invitation.get().authCode!;
+  console.log('DXOS share invitation', code, authCode);
+  return { space, code, authCode };  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
+import { readFileSync, writeFileSync } from "fs";
 import { getIssue } from "./github.js";
 import { convertIssue } from "./cambria.js";
-import { initDxos } from "./dxos.js";
+import { acceptInvite, generateInvite, initDxos } from "./dxos.js";
 
 async function run(): Promise<void> {
-  const doc = await getIssue();
-  console.log(doc);
-  const newDoc = await convertIssue(doc);
-  console.log(newDoc);
-  await initDxos();
+  let invite: { code: string, authCode: string } | undefined = undefined;
+  try {
+    const buff = readFileSync('space-key.json');
+    invite = JSON.parse(buff.toString());
+  } catch {
+  }
+  const dxosClient = await initDxos();
+  let space: object | undefined = undefined;
+  if (typeof invite === undefined) {
+    const result: { space: object, code: string, authCode: string } = await generateInvite(dxosClient);
+    invite = { code: result.code, authCode: result.authCode };
+    writeFileSync('space-key.json', JSON.stringify(invite));
+  } else {
+    await acceptInvite(dxosClient, invite?.code!, invite?.authCode!);
+  }
+  // const doc = await getIssue();
+  // console.log(doc);
+  // const newDoc = await convertIssue(doc);
+  // console.log(newDoc);
+  process.exit(0);
 }
 
 // ...
